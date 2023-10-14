61400ffcce0d09face3121202b040e2c95298bc9
Krishna-Bhagat <krishnabhagat211@gmail.com>
2023-10-14 15:42:20 +0530

    .

---
+++ Basic java Programs/inverse_of_num.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class inverse_of_num {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter any number:  ");
+        int num=sc.nextInt();
+        int result=0;
+        boolean flag=true;
+
+        String tem=Integer.toString(num);
+        for(int i=1;i<=tem.length();i++){
+            String tem_char=Integer.toString(i);
+            if(tem.indexOf(tem_char)==-1){
+                System.out.println("Sorry! Your number is not prefferable for inverse.");
+                flag=false;
+                break;
+            }
+        }
+
+        if(flag){
+            for(int i=1;i<=tem.length();i++){
+                int rem=num%10;
+                num/=10;
+                result+=(i*Math.pow(10, rem-1));
+            }
+            System.out.println("Your inverse is: "+result);
+        }
+        sc.close();
+    }
+}
